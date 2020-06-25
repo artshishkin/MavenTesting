a0685bf61ca66e2f1fb2c877ab18463de5619272
Art <d.art.shishkin@gmail.com>
2020-06-25 19:29:34 +0300

    added simple Java HelloWorld class, application.properties files

---
+++ src/main/java/com/artarkatesoft/maventesting/JavaHelloWorld.java
@@ -0,0 +1,9 @@
+package com.artarkatesoft.maventesting;
+
+public class JavaHelloWorld {
+    private String message = "Hello Art";
+
+    public String getMessage() {
+        return message;
+    }
+}
